474345aa41205afff6a807d65b141892d1938e5e
Oliver <oliver@pop-os.localdomain>
2023-03-01 19:20:11 +0000

    Modified hangman.py

--- Python3/hangman/hangman.py
+++ Python3/hangman/hangman.py
@@ -44,13 +44,13 @@ Etc...
 
 """
 
-
 # need func to pass json to dict
 # func to randomly select a word and definition
 # func to split randomly selected word into list or tuple of chars 
 # func to prompt to take user input and compare against whether char in answer and if user has enough goes left
 #  
 import json
+import random
 
 def json_to_dict(filename):
     with open(filename, 'r') as json_file:
@@ -58,28 +58,101 @@ def json_to_dict(filename):
     
     data = json.loads(json_string)
     result = {item['word']: item['definition'] for item in data}
-
     return result
+
+def select_random(filename):
+    with open(filename, 'r') as file:
+        json_data = json.load(file)
+        random_entry = random.choice(json_data)
+        word = random_entry['word']
+        definition = random_entry['definition']
+        wordlist_dict = {word: definition}
+        return wordlist_dict
+
+def game_config(wordlist_dict):
+    for key, value in wordlist_dict.items():
+        word = key
+        definition = value
+        word_length = len(word)
+        word_to_tuple = tuple(word)
+        underscores = "_" * word_length
+        underscores_to_list = list(underscores)
+    return word, definition, word_to_tuple, underscores_to_list
+
+def char_to_list(user_input, word_to_tuple, underscores_to_list):
+    # Check if user_input is inside wordlist_dict
+    # if true, get position in tuple, e.g. [2]
+    # replace underscores_to_list[2] with word_to_tuple[2] 
+    indices = [i for i, x in enumerate(word_to_tuple) if x == user_input]
+    for index in indices:
+        underscores_to_list[index] = user_input
+    return underscores_to_list
 
 def main_menu():
     return 0
 
 def main():    
+    debugging = False # Set true for verbose print messages
     wordlist_dict = {}
+    word = ""
+    definition = ""
+    word_to_tuple = ()
+    underscores_to_list = []
     hangman_logo = ["HANGMAN"]
-    
     menu_ui = [
-
+        '[1] Singleplayer',
+        '[2] Multiplayer',
+        '[Q] Exit Game'
     ]
+    menu_choice = ''
+    lives = 20
+    oxford_dictionary = "oxford_dictionary.json"
+    turn_count = 1
 
-    while True:
+    while menu_choice == '':
         for i in menu_ui:
             print(i)
+        menu_choice = input("Enter the gamemode you wish to play. $: ")
 
-    wordlist_dict = json_to_dict("oxford_dictionary.json")
-    print(wordlist_dict)
+        while menu_choice == '1':
+            print("[*] Singplayer mode selected")
+            print("[*] Generating word challenge...")
+            wordlist_dict = select_random(oxford_dictionary)
+            if debugging == True:
+                print(wordlist_dict)
+            print("[+] Success!\n")
+            word, definition, word_to_tuple, underscores_to_list = game_config(wordlist_dict)
+            print(word, definition, word_to_tuple, underscores_to_list)
 
-    
+            while lives >= 1 and "_" in underscores_to_list:
+                for i in word_to_tuple:
+                    print(f"\nLives remaining: {lives}")
+                    if debugging == True:
+                        print(word_to_tuple)
+                    print(" ".join(underscores_to_list))
+                    user_input = input("Enter the letter you wish to try. $: ")
+                    underscores_to_list = char_to_list(user_input, word_to_tuple, underscores_to_list)
+                    lives -= 1
+                    # Function that swaps char in tuple with _ in list and deducts a life from lives variable
+                
+            if lives == 0 and "_" in underscores_to_list:
+                print("Wah wah, you've lost...")
+                print("The word was:", word + "\n which means", definition)
+                menu_choice = ''
+
+            elif not "_" in underscores_to_list:
+                print("You win!")
+                print("The word was:", word, "which means:", definition)
+                menu_choice = ''
+    if menu_choice == 'Q' or menu_choice == 'q':
+        print("[*] Quitting...")
+        exit()
+
+    else:
+        print("[!] Error, invalid option.")
+        menu_choice = ''
 
 if __name__ == "__main__":
     main()
+
+
